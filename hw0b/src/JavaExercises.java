49eabb24fef96f10e30d0637b49812a2a832116d
sena <senaizumi675@gmail.com>
2025-07-21 23:54:52 +0800

    Completed hw0b

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -1,4 +1,8 @@
+import edu.princeton.cs.algs4.In;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class JavaExercises {
@@ -6,7 +10,7 @@ public class JavaExercises {
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
         // TODO: Fill in this function.
-        return null;
+        return new int[]{1, 2, 3, 4, 5, 6};
     }
 
     /** Returns the order depending on the customer.
@@ -15,14 +19,24 @@ public class JavaExercises {
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
         // TODO: Fill in this function.
-        return null;
+        if(customer.equals("Ergun")) {
+            return new String[] {"beyti", "pizza", "hamburger", "tea"};
+        }
+        else if(customer.equals("Erik")) {
+            return new String[] {"sushi", "pasta", "avocado", "coffee"};
+        }
+        else {
+            return new String[3];
+        }
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
         // TODO: Fill in this function.
-        return 0;
+        int result = 0;
+        result = Arrays.stream(array).max().getAsInt() - Arrays.stream(array).min().getAsInt();
+        return result;
     }
 
     /**
@@ -39,7 +53,18 @@ public class JavaExercises {
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
         // TODO: Fill in this function.
-        return null;
+        while(x != 1) {
+            list.add(x);
+            if(x % 2 == 0) {
+                x /= 2;
+            }
+            else {
+                x *= 3;
+                ++x;
+            }
+        }
+        list.add(1);
+        return list;
     }
 
 }
